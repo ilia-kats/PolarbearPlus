a0af3b9dc3cf458793d47dab7ae046b06db1ffa6
Ilia Kats <ilia-kats@gmx.net>
2024-02-12 20:50:11 +0100

    add messenger to scale the KL divergence term (required for beta-VAE)

---
+++ src/polarbearplus/_vae/scalelatentmessenger.py
@@ -0,0 +1,32 @@
+from collections.abc import Callable
+
+from pyro.poutine.messenger import Messenger
+
+
+class ScaleLatentMessenger(Messenger):
+    """Scale the score of all unobserved sample sites.
+
+    This is useful when implementing a beta-VAE.
+
+    Args:
+        scale: The scale (beta)
+    """
+
+    def __init__(self, scale: float):
+        super().__init__()
+        self._scale = scale
+
+    def _pyro_post_sample(self, msg):
+        if not msg["is_observed"]:
+            msg["scale"] *= self._scale
+
+
+def scale_latent(fn: Callable = None, scale: float = 1.0):
+    """Convenient wrapper of `.ScaleLatentMessenger`.
+
+    Args:
+        fn: A stochastic function (callalbe containing Pyro primitive calls)
+        scale: The scale.
+    """
+    msngr = ScaleLatentMessenger(scale=scale)
+    return msngr(fn) if fn is not None else msngr
