ed7b07c6a11bfe5418e9f0fd8138040cfb69f63c
Ilia Kats <ilia-kats@gmx.net>
2024-02-24 16:06:19 +0100

    ATAC datamodule fixes - calculate the peak indices as required by model - binarize peak data - proper dtype for batch_idx - remove unnecessary logbatchmeans and logbatchvars - add progress bar to downloads - simplify code - don't re-run setup code if called multiple times on the same object

--- datamodules/singleassaydatamodule.py
+++ datamodules/singleassaydatamodule.py
@@ -1,13 +1,30 @@
 import os
+import urllib.request
 
 import lightning as L
 import numpy as np
 import pandas as pd
-import requests
 import torch
 from scipy.io import mmread
 from scipy.sparse import vstack
 from torch.utils.data import DataLoader, Dataset, TensorDataset, random_split
+from tqdm.auto import tqdm
+
+
+class _TqdmDownload(tqdm):
+    def __init__(self, *args, **kwargs):
+        kwargs = dict(kwargs)
+        kwargs.update({"unit": "B", "unit_scale": True, "unit_divisor": 1024})
+        super().__init__(*args, **kwargs)
+
+    def update_to(self, nblocks=1, blocksize=1, total=-1):
+        self.total = total
+        self.update(nblocks * blocksize - self.n)
+
+
+def _download(url, outfile, desc):
+    with _TqdmDownload(desc="downloading " + desc) as t:
+        urllib.request.urlretrieve(url, outfile, t.update_to)  # noqa S310
 
 
 class AtacCounts(Dataset):
@@ -34,8 +51,8 @@ class AtacCounts(Dataset):
 
     def __getitem__(self, idx):
         """Return the data and batch info for idx number of samples."""
-        idx = idx.tolist() if torch.is_tensor(idx) else idx
-        data = torch.Tensor(self.counts[idx, :].toarray())
+        idx = idx.numpy() if torch.is_tensor(idx) else idx
+        data = self.counts[idx, :].toarray()
         batch = self.batch_info[idx]
         return data.squeeze(), batch
 
@@ -49,104 +66,54 @@ class AtacDataModule(L.LightningDataModule):
     We concatenate the two datasets along the cell dimension and we add a vector of batch indices
     to the output of our TensorDataset.
 
-    Attributes:
-    ----------
-    files : list
-        a list of strings containing the names of the files to download
-    base_url : str
-        the base url to download the files from
-    seed : int
-        set a random seed for train/val/test split
-    data_dir : str
-        directory to save all files
-    num_cells : int
-        number of cells in the dataset
-    num_peaks : int
-        number of peak in the dataset
-    peaks : pd.DataFrame
-        peak metadata (genomic coordiantes)
-    logbatchmean : np.array
-        mean of library size for each batch
-    logbatchvar : np.array
-        variance of library size for each batch
-
+    Args:
+        batch_size: Minibatch size.
+        n_workers: Number of dataloader workers.
+        pin_memory: Whether to use pinned memory.
+        data_dir: directory to save all files
+        seed: set a random seed for train/val/test split
     """
 
-    files = [
-        "adultbrainfull50_atac_outer_single.mtx",  # Single assay RNA-seq
-        "adultbrainfull50_atac_outer_snareseq.mtx",
-        "adultbrainfull50_atac_outer_snareseq_barcodes.tsv",  # SNARE-seq cells
-        "adultbrainfull50_atac_outer_single_barcodes.tsv",  # single assay cells
-        "adultbrainfull50_atac_outer_peaks.txt",
-    ]
-    base_url = "https://noble.gs.washington.edu/~ranz0/Polarbear/data/"
+    _files = {
+        "single": "adultbrainfull50_atac_outer_single.mtx",
+        "snareseq": "adultbrainfull50_atac_outer_snareseq.mtx",
+        "snareseq_barcodes": "adultbrainfull50_atac_outer_snareseq_barcodes.tsv",
+        "single_barcodes": "adultbrainfull50_atac_outer_single_barcodes.tsv",
+        "peak_annotation": "adultbrainfull50_atac_outer_peaks.txt",
+    }
+    _base_url = "https://noble.gs.washington.edu/~ranz0/Polarbear/data/"
 
-    def __init__(self, data_dir: str = "./data/snareseq"):
-        """Parameters.
-
-        ----------
-        seed : int
-            set a random seed for train/val/test split
-        data_dir : str
-            directory to save all files
-        num_cells : int
-            number of cells in the dataset
-        num_genes : int
-            number of genes in the dataset
-        genes : pd.DataFrame
-            gene metadata (gene names)
-        logbatchmean : np.array
-            mean of library size for each batch
-        logbatchvar : np.array
-            variance of library size for each batch
-        """
+    def __init__(
+        self,
+        batch_size: int,
+        n_workers: int = 0,
+        pin_memory: bool = False,
+        data_dir: str = "./data/snareseq",
+        seed: int = 42,
+    ):
         super().__init__()
-        self.seed = 42
-        self.data_dir = data_dir
+        self._batch_size = batch_size
+        self._n_workers = n_workers
+        self._pin_memory = pin_memory
+
+        self._seed = seed
+        self._data_dir = data_dir
         self._num_cells = None
         self._num_genes = None
-        self._genes = None
-        self._logbatchmean = None
-        self._logbatchvar = None
+        self._chr_idx = None
+
+        self._dset_train = self._dset_test = self._dset_val = None
 
     def prepare_data(self):
         """Download data and prepare train/test/val split."""
-        for filename in self.files:
-            url = f"{self.base_url}{filename}"
-            if os.path.isfile(os.path.join(self.data_dir, filename)):
-                print(f"{filename} already downloaded")
+        for filedesc, filename in self._files.items():
+            url = f"{self._base_url}{filename}"
+            filepath = os.path.join(self._data_dir, filename)
+            if os.path.isfile(filepath):
+                print(f"{filepath} already downloaded")
             else:
-                if os.path.isdir(self.data_dir):
-                    pass
-                else:
-                    os.makedirs(self.data_dir)
-
-                try:
-                    r = requests.get(url, timeout=10)
-                    with open(os.path.join(self.data_dir, filename), "wb") as f:
-                        f.write(r.content)
-                    print(f"Downloaded {filename}")
-
-                except requests.exceptions.RequestException:
-                    print(f"Failed to download {url}")
-
-        # prepare the random train/test/val split
-        cells1 = pd.read_csv(
-            os.path.join(self.data_dir, "adultbrainfull50_atac_outer_snareseq_barcodes.tsv"), sep="\t", header=0
-        )
-        cells2 = pd.read_csv(
-            os.path.join(self.data_dir, "adultbrainfull50_atac_outer_single_barcodes.tsv"), sep="\t", header=0
-        )
-        cells1["batch"]
-        cells = pd.concat([cells1, cells2], axis=0)
-
-        train, test, val = random_split(
-            torch.Tensor(cells.index), [0.6, 0.2, 0.2], generator=torch.Generator().manual_seed(self.seed)
-        )
-        for i, name in zip(
-            [train.indices, test.indices, val.indices], ["atac_train", "atac_test", "atac_val"], strict=False
-        ):
-            torch.save(i, os.path.join(self.data_dir, f"{name}.pt"))
+                os.makedirs(self._data_dir, exist_ok=True)
+                _download(url, filepath, filedesc)
 
     @property
     def num_peaks(self):
@@ -154,87 +121,81 @@ class AtacDataModule(L.LightningDataModule):
         return self._num_peaks
 
     @property
-    def num_cells(
-        self,
-    ):
+    def num_cells(self):
         """Number of cells in the dataset."""
         return self._num_cells
 
     @property
-    def peaks(self):
-        """Peak metadata (genomic coordinates)."""
-        return self._peaks
+    def num_batches(self):
+        """Number of experimental batches (datasets)."""
+        return 2
 
     @property
-    def logbatchnorm(self):
-        """Mean of library size for each batch."""
-        return self._logbatchnorm
+    def chromosome_indices(self):
+        """List of index ranges in the data matrix belonging to separate chromosomes."""
+        return self._chr_idx
 
-    @property
-    def logbatchvar(self):
-        """Variance of library size for each batch."""
-        return self._logbatchvar
-
-    def setup(self, stage: str):
+    def setup(self, stage: str | None = None):
         """Load Dataset and assign train/val/test datasets for use in dataloaders."""
-        atac_counts1 = mmread(os.path.join(self.data_dir, "adultbrainfull50_atac_outer_snareseq.mtx"))
-        atac_counts2 = mmread(os.path.join(self.data_dir, "adultbrainfull50_atac_outer_single.mtx"))
-        # concatenate along cell dimension, first g columns correspond to genes, last p columns correspond to peaks
-        counts = vstack((atac_counts1, atac_counts2))
-        counts = counts.astype(np.float32)
-        counts = counts.tocsr()  # convert to compressed sparse row format
+        if self._chr_idx is None or self._dset_train is None or self._dset_val is None or self._dset_test is None:
+            atac_counts1 = mmread(os.path.join(self._data_dir, self._files["snareseq"]))
+            atac_counts2 = mmread(os.path.join(self._data_dir, self._files["single"]))
+            # concatenate along cell dimension, first g columns correspond to genes, last p columns correspond to peaks
+            counts = vstack((atac_counts1, atac_counts2))
+            counts = counts.astype(np.float32)
+            counts = counts.tocsr()  # convert to compressed sparse row format
+            counts.data[:] = 1
 
-        # create batch index for the two data sets
-        batch_info = torch.from_numpy(
-            np.concatenate((np.zeros(atac_counts1.shape[0]), np.ones(atac_counts2.shape[0])), axis=0)
-        )
-
-        self._num_cells = counts.shape[0]
-        self._num_peaks = counts.shape[1]
-        self._peaks = pd.read_csv(
-            os.path.join(self.data_dir, "adultbrainfull50_atac_outer_peaks.txt"), sep="\t", header=None
-        )
-
-        # Compute mean and variance of library size for each batch
-        library_size1, library_size2 = np.log(atac_counts1.sum(axis=1)), np.log(atac_counts2.sum(axis=1))
-        self._logbatchmean = np.array([library_size1.mean(), library_size2.mean()])
-        self._logbatchvar = np.array([library_size1.var(), library_size2.var()])
-
-        if stage == "fit":
-            # Assign train/val datasets for use in dataloaders
-            train_ind, val_ind = (
-                torch.load(os.path.join(self.data_dir, "atac_train.pt")),
-                torch.load(os.path.join(self.data_dir, "atac_val.pt")),
+            # create batch index for the two data sets
+            batch_info = torch.cat(
+                (
+                    torch.zeros(atac_counts1.shape[0], dtype=torch.int64),
+                    torch.ones(atac_counts2.shape[0], dtype=torch.int64),
+                )
             )
-            print(counts[train_ind].shape, batch_info[train_ind].shape)
-            print(train_ind.shape, val_ind.shape)
-            self.atac_train = AtacCounts(counts[train_ind], batch_info[train_ind])
-            self.atac_val = AtacCounts(counts[val_ind], batch_info[val_ind])
 
-        # Assign test dataset for use in dataloader(s)
-        if stage == "test":
-            test_ind = torch.load(os.path.join(self.data_dir, "atac_test.pt"))
-            self.atac_test = AtacCounts(counts[test_ind], batch_info[test_ind])
+            dset = AtacCounts(counts, batch_info)
+            self._dset_train, self._dset_val, self._dset_test = random_split(
+                dset, [0.6, 0.2, 0.2], generator=torch.Generator().manual_seed(self._seed)
+            )
 
-        if stage == "predict":
-            test_ind = torch.load(os.path.join(self.data_dir, "atac_test.pt"))
-            self.atac_predict = AtacCounts(counts[test_ind], batch_info[test_ind])
+            self._num_cells = counts.shape[0]
+            self._num_peaks = counts.shape[1]
+            peaks = pd.read_table(
+                os.path.join(self._data_dir, self._files["peak_annotation"]),
+                sep=":",
+                header=None,
+                names=["chr", "range"],
+            )
+            chromosomes, peak_indices, peak_counts = np.unique(
+                peaks.chr.to_numpy(), return_index=True, return_counts=True
+            )
+            chrorder = np.argsort(peak_indices)
+            chromosomes, peak_indices, peak_counts = (
+                chromosomes[chrorder],
+                peak_indices[chrorder],
+                peak_counts[chrorder],
+            )
+            self._chr_idx = [(idx, idx + cnt) for idx, cnt in zip(peak_indices, peak_counts, strict=False)]
+
+    def _get_dataloader(self, dset):
+        return DataLoader(dset, batch_size=self._batch_size, num_workers=self._n_workers, pin_memory=self._pin_memory)
 
     def train_dataloader(self):
         """Train dataloader."""
-        return DataLoader(self.atac_train, batch_size=32)
+        return self._get_dataloader(self._dset_train)
 
     def val_dataloader(self):
         """Validation dataloader."""
-        return DataLoader(self.atac_val, batch_size=32)
+        return self._get_dataloader(self._dset_val)
 
     def test_dataloader(self):
         """Test dataloader."""
-        return DataLoader(self.atac_test, batch_size=32)
+        return self._get_dataloader(self._dset_test)
 
     def predict_dataloader(self):
         """Predict dataloader."""
-        return DataLoader(self.atac_predict, batch_size=32)
+        return self._get_dataloader(self._dset_test)
 
 
 class RnaDataModule(L.LightningDataModule):
@@ -314,22 +275,12 @@ class RnaDataModule(L.LightningDataModule):
         """Download data and prepare train/test/val split."""
         for filename in self.files:
             url = f"{self.base_url}{filename}"
-            if os.path.isfile(os.path.join(self.data_dir, filename)):
-                print(f"{filename} already downloaded")
+            filepath = os.path.join(self.data_dir, filename)
+            if os.path.isfile(filepath):
+                print(f"{filepath} already downloaded")
             else:
-                if os.path.isdir(self.data_dir):
-                    pass
-                else:
-                    os.makedirs(self.data_dir)
-
-                try:
-                    r = requests.get(url, timeout=10)
-                    with open(os.path.join(self.data_dir, filename), "wb") as f:
-                        f.write(r.content)
-                    print(f"Downloaded {filename}")
-
-                except requests.exceptions.RequestException:
-                    print(f"Failed to download {url}")
+                os.makedirs(self.data_dir, exist_ok=True)
+                _download(url, filepath, filepath)
 
         # prepare the random train/test/val split
         cells1 = pd.read_csv(
@@ -342,7 +293,7 @@ class RnaDataModule(L.LightningDataModule):
         cells = pd.concat([cells1, cells2], axis=0)
 
         train, test, val = random_split(
-            torch.Tensor(cells.index), [0.6, 0.2, 0.2], generator=torch.Generator().manual_seed(self.seed)
+            torch.Tensor(cells.index), [0.6, 0.2, 0.2], generator=torch.Generator().manual_seed(self._seed)
         )
         for i, name in zip(
             [train.indices, test.indices, val.indices], ["rna_train", "rna_test", "rna_val"], strict=False
