5f4363bbd0a159b5743fb16ec8b7027bb8a8f462
Ilia Kats <ilia-kats@gmx.net>
2024-03-13 20:01:27 +0100

    statswriter: open output file on first write

--- src/polarbearplus/_utils/statswriter.py
+++ src/polarbearplus/_utils/statswriter.py
@@ -6,16 +6,18 @@ from lightning.pytorch.callbacks import BasePredictionWriter
 
 
 class StatsWriter(BasePredictionWriter):
-    def __init__(self, output_dir: str | os.PathLike, chunk_max_mp=20):
+    def __init__(self, output_dir: str | os.PathLike, chunk_max_mb=20):
         super().__init__("batch")
         self._output_path = os.path.join(output_dir, "stats.zarr")
-        self._chunk_max_mb = 20
-
-        self._zarrstore = zarr.group(self._output_path, overwrite=True)
+        self._chunk_max_mb = chunk_max_mb
+        self._zarrstore = None
 
     def write_on_batch_end(self, trainer, pl_module, prediction, batch_indices, batch, batch_idx, dataloader_idx):
         if not isinstance(prediction, dict):
             raise ValueError(f"{self.__class__.__name__} only works with predictions that are dicts of arrays.")
+        if self._zarrstore is None:
+            self._zarrstore = zarr.group(self._output_path, overwrite=True)
+
         for k, s in prediction.items():
             if k not in self._zarrstore:
                 ssize = s.nbytes / 1024**2
@@ -30,3 +32,7 @@ class StatsWriter(BasePredictionWriter):
                 self._zarrstore.create_dataset(k, data=s, chunks=chunkshape)
             else:
                 self._zarrstore[k].append(s, axis=0)
+
+    def on_predict_end(self):
+        if self._zarrstore is not None:
+            self._zarrstore.close()
